6309937cf465d5305c092c171f4c8dec0c658e0a
shoaibbhimani <shoaibbhimani1392@gmail.com>
2020-09-18 23:51:54 +0530

    complete post manage

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,9 +1,25 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/core";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Get } from "../Utils/JSONUtil";
+import { Get, Put } from "../Utils/JSONUtil";
+
+import PostManage from "../components/PostManage";
+
 const PostDetail = () => {
   const { postId } = useParams();
   const [postDetail, setPostDetail] = useState(null);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     Get(`http://localhost:3002/posts/${postId}`).then((response) => {
@@ -11,9 +27,48 @@ const PostDetail = () => {
     });
   }, [postId]);
 
+  const onSubmitHandler = (values) => {
+    console.log(values);
+    // Put Request
+    Put(`http://localhost:3002/posts/${postId}`, values)
+      .then(() => {
+        onClose();
+        getPostDetail();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // Modal Close
+  };
+
   return (
     <div>
-      {postDetail === null ? <div>Loading</div> : postDetail.description}
+      {postDetail === null ? (
+        <div>Loading</div>
+      ) : (
+        <div>
+          <Button onClick={onOpen}>Edit</Button>
+          <div>{postDetail.description}</div>
+
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalCloseButton />
+              <ModalHeader>Post Manage</ModalHeader>
+              <ModalBody>
+                <PostManage
+                  onSubmit={onSubmitHandler}
+                  defaultTitle={postDetail.title}
+                  defaultDescription={postDetail.description}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button onClick={onClose}>Close</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </div>
+      )}
     </div>
   );
 };
